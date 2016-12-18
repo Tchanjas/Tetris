0292c24e8eb8cd2ded080227cce466f0e33f8149
Bruno Freitas <l0custdev@gmail.com>
2016-12-18 22:22:53 +0000

    Implement procedure to select piece at random Minor code fixes

--- base2.asm
+++ base2.asm
@@ -1,187 +1,243 @@
-STACK SEGMENT PARA STACK ; define the stack segment
-  DB 64 DUP('MYSTACK')
-STACK ENDS
+stack segment para stack ; define the stack segment
+db 64 dup('mystack')
+stack ends
 
-MYDATA SEGMENT PARA 'DATA'
-  DB 64 DUP('MYSTACK')
-  WAIT_TIME DW 1
-  ; game matrix 320x200 divived by 20px for each blocks gives us 16x10 but we
-  ; want the last row to have information so it's 16x9 = 144
-  matrix DB 144 DUP(0), '$'
-  matrixIndexLength DW 143, '$' ; 143 because it starts at 0
-  matrixLineLength DB 16, '$'
+mydata segment para 'data'
+db 64 dup('mystack')
+wait_time dw 1
+; game matrix 320x200 divived by 20px for each blocks gives us 16x10 but we
+; want the last row to have information so it's 16x9 = 144
+matrix db 144 dup(0), '$'
+matrixindexlength dw 143, '$' ; 143 because it starts at 0
+matrixlinelength db 16, '$'
 
-  draw DW ?, '$'
-  drawIndex DB 0, '$'
-  drawIndexLength DW 0, '$'
-  drawLineLength DB 0, '$'
+draw dw ?, '$'
+drawindex db 0, '$'
+drawindexlength dw 0, '$'
+drawlinelength db 0, '$'
 
-  CurrentBlock_X DW 0, '$'
-  CurrentBlock_Y DW 0, '$'
+currentblock_x dw 0, '$'
+currentblock_y dw 0, '$'
 
-  ; declare the matrix pieces
-  ; every piece is 4x2
-  pieceLineFour DB 0, 0, 0, 0, 1, 1, 1, 1, '$'
-  pieceLineTwo  DB 0, 0, 0, 0, 0, 1, 1, 0, '$'
-  pieceLUp      DB 1, 0, 0, 0, 1, 1, 1, 0, '$'
-  pieceLDown    DB 1, 1, 1, 0, 1, 0, 0, 0, '$'
-  pieceZ        DB 1, 1, 0, 0, 0, 1, 1, 0, '$'
-  
-  currentPiece  DB ?, '$'
-  currentPieceX  DB 0, '$'
-  currentPieceY  DB 0, '$'
-MYDATA ENDS
+; declare the matrix pieces
+; every piece is 4x2
+piecelinefour db 1, 1, 1, 1, 0, 0, 0, 0, '$'
+piecelinetwo  db 1, 1, 0, 0, 0, 0, 0, 0, '$'
+piecelup      db 1, 0, 0, 0, 1, 1, 1, 0, '$'
+pieceldown    db 1, 1, 1, 0, 1, 0, 0, 0, '$'
+piecez        db 1, 1, 0, 0, 0, 1, 1, 0, '$'
 
-MYCODE SEGMENT PARA 'CODE' ; define the code segment
-MYPROC PROC FAR ; name of the procedure MYPROC
-  ASSUME CS:MYCODE, DS:MYDATA, SS:STACK
-  PUSH DS ; store in the stack the segment DS
-  XOR AX, AX ; guarantee zero in AX
-  PUSH AX ; store zero in the stack
-  MOV AX, MYDATA
-  MOV DS, AX
+currentpiece  db ?, '$'
+currentpiecex db 0, '$'
+currentpiecey db 0, '$'
+mydata ends
 
-  ; ---- graphics mode
-  MOV AH, 00h ; prepare to define the graphical mode
-  MOV AL, 04h ; graphic mode 320x200 color mode
-  INT 10h ; 10h interruption
+mycode segment para 'code' ; define the code segment
+myproc proc far ; name of the procedure myproc
+assume cs:mycode, ds:mydata, ss:stack
+	push ds ; store in the stack the segment ds
+	xor ax, ax ; guarantee zero in ax
+	push ax ; store zero in the stack
+	mov ax, mydata
+	mov ds, ax
 
-  ; ---- background
-  MOV AH, 11 ; prepare color palette config
-  MOV BH, 00 ; initialize the background color
-  MOV BL, 07 ; light grey background
-  INT 10h
+	; ---- graphics mode
+	mov ah, 00h ; prepare to define the graphical mode
+	mov al, 04h ; graphic mode 320x200 color mode
+	int 10h ; 10h interruption
 
-  ; ---- foreground
-  MOV AH, 11 ; prepare color palette config
-  MOV BH, 01 ; initialize the foreground color
-  MOV BL, 06 ; red, black and green
-  INT 10h
+	; ---- background
+	mov ah, 11 ; prepare color palette config
+	mov bh, 00 ; initialize the background color
+	mov bl, 00 ; black background
+	int 10h
 
-  ; change a element in the matrix just so we can test if it works
-  MOV BL, 1
-  MOV matrix[68], BL
+	; ---- foreground
+	mov ah, 11 ; prepare color palette config
+	mov bh, 01 ; initialize the foreground color
+	mov bl, 06 ; red, black and green
+	int 10h
 
-  ; ---- draw the elements of background matrix
-  MOV AX, offset matrix ; get address of matrix 
-  MOV draw, AX
-  MOV AX, matrixIndexLength ; size of the matrix
-  MOV drawIndexLength, AX
-  MOV AL, matrixLineLength ; size for each line of the matrix
-  MOV drawLineLength, AL
-  CALL drawMatrix
+	; change a element in the matrix just so we can test if it works
+	;mov bl, 1
+	;mov matrix[68], bl
 
-  ; ---- draw the elements of a piece matrix
-  MOV currentPieceX, 5
-  MOV currentPieceY, 5
-  MOV AX, offset pieceZ
-  MOV draw, AX
-  MOV drawIndexLength, 7
-  MOV drawLineLength, 4
-  CALL drawMatrix
+	; ---- draw the elements of background matrix
+	mov ax, offset matrix ; get address of matrix 
+	mov draw, ax
+	mov ax, matrixindexlength ; size of the matrix
+	mov drawindexlength, ax
+	mov al, matrixlinelength ; size for each line of the matrix
+	mov drawlinelength, al
+	call drawmatrix
 
-  ; ---- waits for a key to end it
-  MOV AH, 01
-  INT 21h
+	; ---- draw the elements of a piece matrix
+	;mov currentpiecex, 5
+	;mov currentpiecey, 5
 
-  ; ---- back to de text mode
-  MOV AH, 00h ; define the graphic mode
-  MOV AL, 02h ; text mode 80x25
-  INT 10h
+	call randompiece
+	mov draw, ax
+	mov drawindexlength, 7
+	mov drawlinelength, 4
+	call drawmatrix
 
-  RET ; return the control to DOS
-MYPROC ENDP ; end of the procedure MYPROC
+	; ---- waits for a key to end it
+	mov ah, 01
+	int 21h
+
+	; ---- back to de text mode
+	mov ah, 00h ; define the graphic mode
+	mov al, 02h ; text mode 80x25
+	int 10h
+
+	ret ; return the control to dos
+myproc endp ; end of the procedure myproc
 
 ; ---- procedure to draw the elements of the a specified matrix
-drawMatrix PROC NEAR
+drawmatrix proc near
+	xor si, si
+matrix_loop:
+	; get coordinates
+	; get width and height based on the index
+	xor ax, ax
+	xor bx, bx
+	xor cx, cx
+	xor dx, dx
 
-MOV SI, 0
+	mov al, drawindex
+	mov bl, drawlinelength
+	div bl ; bx bl
+	mov cl, ah ; width
+	mov dl, al ; height
 
-  matrix_loop:
-  ; get coordinates
-  ; get width and height based on the index
-  xor ax, ax
-  xor bx, bx
-  xor cx, cx
-  xor dx, dx
+	; multiply to get the real coordinates
+	mov ax, dx
+	mov bx, 20
+	mul bx
+	mov currentblock_y, ax
+	mov ax, cx
+	mul bx
+	mov currentblock_x, ax
 
-  mov al, drawIndex
-  mov bl, drawLineLength
-  div bl ; bx bl
-  mov cl, ah ; width
-  mov dl, al ; height
+	; print block
+	mov di, draw
+	add di, si
+	mov bl, [di]
+	cmp bl, 1
+	jne matrix_loop_updateindex
+	call printblock
 
-  ; multiply to get the real coordinates
-  mov ax, dx
-  mov bx, 20
-  mul bx
-  mov CurrentBlock_Y, ax
-  mov ax, cx
-  mul bx
-  mov CurrentBlock_X, ax
+	; update index
+matrix_loop_updateindex:
+	xor ax, ax
+	xor bx, bx
+	xor cx, cx
+	xor dx, dx
+	mov al, drawindex
+	inc al
+	mov drawindex, al
+	inc si
 
-  ; print block
-  mov di, draw
-  add di, si
-  mov bl, [di]
-  cmp bl, 1
-  jne matrix_loop_UpdateIndex
-  call printBlock
+	; check if finished
+	mov bx, drawindexlength
+	cmp ax, bx
+	jle matrix_loop
 
-  ; update index
-  matrix_loop_UpdateIndex:
-  xor ax, ax
-  xor bx, bx
-  xor cx, cx
-  xor dx, dx
-  mov al, drawIndex
-  inc al
-  mov drawIndex, al
-  inc si
-
-  ; check if finished
-  mov bx, drawIndexLength
-  cmp ax, bx
-  jle matrix_loop
-
-  mov drawIndex, 0
-  mov drawIndexLength, 0
-  mov drawLineLength, 0
-  RET
-drawMatrix ENDP
+	mov drawindex, 0
+	mov drawindexlength, 0
+	mov drawlinelength, 0
+	ret
+drawmatrix endp
 
 ; ---- procedure that draws a defined block of width and height in specific coordinates
-printBlock PROC NEAR
-; ---- display pixels
-mov dx, CurrentBlock_Y
-add dx, 20
-mov CurrentBlock_Y, dx
-sub dx, 20
+printblock proc near
+	; ---- display pixels
+	add currentblock_y, 20
+	mov dx, currentblock_y
+	sub dx, 20
 
-mov cx, CurrentBlock_X
-add cx, 20
-mov CurrentBlock_X, cx
-sub cx, 20
+	add currentblock_x, 20
+	mov cx, currentblock_x
+	sub cx, 20
 
-printBlock_loop_y:
-  printBlock_loop_x:
-    mov al, 02 ; black
-    mov ah, 12 ; config int10h to the pixel plot
-    int 10h
-    inc cx
-    mov bx, CurrentBlock_X
-    cmp cx, bx
-    jle printBlock_loop_x
-  inc dx
-  xor cx, cx
-  mov cx, CurrentBlock_X
-  sub cx, 20
-  mov bx, CurrentBlock_Y
-  cmp dx, bx
-  jle printBlock_loop_y
+	mov al, 03 ; green
+	mov ah, 12 ; config int10h to the pixel plot
 
-  RET
-printBlock ENDP
+	; ---- start drawing block
+drawColumn:
+	int 10h
+	inc cx
+	cmp cx, currentblock_x
+	jb drawColumn
 
-MYCODE ENDS ; end of the code segment
-END ; end of the program
+	sub cx, 20 ; starting x coordinate of current block
+	inc dx ; new line
+	cmp dx, currentblock_y
+	jb drawColumn
+	ret  
+printblock endp
+
+; --- selects a piece at random using system clock ticks
+randompiece proc near
+	push bx
+	push cx
+	push dx
+	; --- system time cx:dx
+	mov ah, 00h
+	int 1ah
+	mov ax, dx
+	xor dx, dx
+	mov cx, 5 
+	div cx
+
+	cmp dx, 0
+	je loadlinefour
+	cmp dx, 1
+	je loadlinetwo
+	cmp dx, 2
+	je loadlup
+	cmp dx, 3
+	je loadldown
+	cmp dx, 4
+	je loadz
+
+loadlinefour:
+	lea ax, piecelinefour
+	jmp exit
+loadlinetwo:
+	lea ax, piecelinetwo
+	jmp exit
+loadlup:
+	lea ax, piecelup
+	jmp exit
+loadldown:
+	lea ax, pieceldown
+	jmp exit
+loadz:
+	lea ax, piecez
+	jmp exit
+
+exit:
+	pop dx
+	pop cx
+	pop bx
+	ret
+randompiece endp
+
+delay proc near
+	push dx
+	push cx
+	timer:
+	mov ah, 00h
+	int 1ah
+	cmp dx,wait_time
+	jb timer
+	add dx,3         ;1-18, where smaller is faster and 18 is close to 1 second
+	mov wait_time,dx
+	pop cx
+	pop dx
+	ret
+delay endp
+
+mycode ends ; end of the code segment
+end ; end of the program
